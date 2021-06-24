3417e16603f0e23ef8ed9f0f6d0fef7993beda90
Yasya <80125891+yasya-ayesha@users.noreply.github.com>
2021-06-24 13:40:08 +0300

    Add files via upload
    added crashing option and score.

--- main.js
+++ main.js
@@ -29,6 +29,8 @@ function getQuantityElements(heightElement) {
 function startGame(){
     start.classList.add('hide');
     gameArea.classList.remove('hide'); 
+    gameArea.innerHTML = '';
+    
 
     for (let i = 0; i < getQuantityElements(100); i++){
         const line = document.createElement('div');
@@ -48,14 +50,21 @@ function startGame(){
         gameArea.appendChild(enemy);
     }
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
+ 
     requestAnimationFrame(playGame);
 }
 
 function playGame(){
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     moveRoad();
     moveEnemy();
     if (setting.start){
@@ -105,6 +114,19 @@ function moveRoad(){
 function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom &&
+            carRect.right >= enemyRect.left && 
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            console.warn('CRASH');
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight;
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y >= document.documentElement.clientHeight){
